88606fc24c103957549d2d7b90d887cf762bc2c0
Aliaksandr Kulba <tunet000@gmail.com>
2022-04-10 15:50:54 +0000

    feat: add security bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class     => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class       => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class               => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
