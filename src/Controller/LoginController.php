9bfb6dff597608090823baa4f2053c479d2aea2c
Aliaksandr Kulba <tunet000@gmail.com>
2022-04-16 19:30:38 +0000

    feat: add form login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+#[Route('/login', name: 'app_login')]
+final class LoginController extends AbstractController
+{
+    public function __construct(
+        private readonly AuthenticationUtils $authenticationUtils,
+    ) {
+    }
+
+    public function __invoke(): Response
+    {
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
