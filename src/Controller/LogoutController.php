6f95fa436a3f03689a3ec275fcef4a597713b4b1
Aliaksandr Kulba <tunet000@gmail.com>
2022-04-17 00:24:20 +0000

    feat: login and logout

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use LogicException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/logout', name: 'app_logout')]
+final class LogoutController extends AbstractController
+{
+    public function __invoke(): void
+    {
+        throw new LogicException('This code should never be reached');
+    }
+}
