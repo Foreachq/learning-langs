2349bb5ac5177d188ad9d60c1ac9d06de27868b8
Aliaksandr Kulba <tunet000@gmail.com>
2022-04-24 00:00:18 +0300

    refactor

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,9 +5,26 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    private function configureContainer(
+        ContainerConfigurator $container,
+        LoaderInterface $loader,
+        ContainerBuilder $builder,
+    ): void {
+        $configDir = $this->getConfigDir();
+
+        $container->import("{$configDir}/{packages}/*.yaml");
+        $container->import("{$configDir}/{packages}/{$this->environment}/*.yaml");
+
+        $container->import("./**/config/services.yaml");
+        $container->import("./**/config/{services}_{$this->environment}.yaml");
+    }
 }
