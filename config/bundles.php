259b2761b10b24dc5e4e3fcc70d1fdc9c2288cf2
Aliaksandr Kulba <tunet000@gmail.com>
2022-05-04 21:53:22 +0300

    refactor

--- config/bundles.php
+++ config/bundles.php
@@ -2,21 +2,13 @@
 
 declare(strict_types=1);
 
-$bundles = [
+return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class             => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class              => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                      => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class        => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class                    => ['dev' => true],
+    Fidry\PsyshBundle\PsyshBundle::class                             => ['dev' => true],
 ];
-
-// check exists require-dev dependencies
-if (class_exists(Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class)) {
-    $bundles = array_merge($bundles, [
-        Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-        Symfony\Bundle\MakerBundle\MakerBundle::class             => ['dev' => true],
-        Fidry\PsyshBundle\PsyshBundle::class                      => ['dev' => true],
-    ]);
-}
-
-return $bundles;
